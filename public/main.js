554d1a53e87fa9b26e5993a4691a6f30e585516d
Hrishikesh <hp02.220@gmail.com>
2024-03-05 01:45:58 +0530

    URLS

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:3000')
+const socket = io.connect('https://chatterupapp.netlify.app')
 
 // elements
 const userList = document.getElementById('users-list')
